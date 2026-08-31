f201a7a625ef07c0f1603c1e6ab58e22092e960f
hailale1 <jorge.a.garcia01@gmail.com>
2026-08-31 08:04:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ ODDS_API_KEY = os.getenv("ODDS_API_KEY", "")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
 
-BASE_ODDS_URL = "https://api.the-odds-api.com/v4/sports/tennis_wta/odds/"
+BASE_ODDS_URL = "https://api.the-odds-api.com/v4/sports/tennis_wta_us_open/odds/"
 DB_NAME = "wta_bot.db"
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
